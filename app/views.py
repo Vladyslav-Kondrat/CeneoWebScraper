518edd5dbeec74cb38b0e7ddfcf6ec32ff494e6c
Vladyslav <236127@student.uek.krakow.pl>
2025-06-02 15:38:35 +0200

    02.06.25 KV

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, url_for, request
 from app.forms import ExtractForm
+from app.models import Product
 
 
 @app.route("/")
@@ -8,7 +9,7 @@ def index():
     return render_template("index.html")
 
 @app.route("/extract")
-def extract():
+def render_form():
     form = ExtractForm()
     return render_template("extract.html", form=form)
 
@@ -17,7 +18,15 @@ def extract():
     form = ExtractForm(request.form)
     if form.validate():
         product_id = form.product_id.data
-        return redirect(url_for('product', product_id=product_id))
+        product = Product(product_id)
+        if product.extract_name():
+            product.extract_opinions()
+            product.analyze()
+            product.export_info()
+            product.export_opinions()
+            return redirect(url_for('product', product_id=product_id))
+        form.product_id.errors.append('There is no product for provided id or product has no opinions')
+        return render_template('extract', form=form)
     return render_template('extract', form=form)
 
 @app.route("/products")
